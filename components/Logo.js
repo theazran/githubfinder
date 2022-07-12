b2041d80ec684756e28d448e78f65f3149b41db8
M ASRAN <62879334+theazran@users.noreply.github.com>
2022-07-12 15:26:34 +0800

    Update

--- components/Logo.js
+++ components/Logo.js
@@ -5,7 +5,7 @@ const Logo = () => {
     <Link href="/">
       <div className="p-3 rounded-xl cursor-pointer">
         <p className="font-mono text-xl font-semibold text-gray-800 dark:text-gray-50 md:text-xl lg:text-2xl">
-          devfinder
+          Github Finder
         </p>
       </div>
     </Link>
